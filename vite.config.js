ca91592b630dbebd05f29dd06562cefdd333a910
Kirian GUILLER <kguiller@questel.com>
2025-12-04 13:33:30 +0100

    chore: add 20mb limit for app

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,11 @@ export default defineConfig({
       }
     }),
     VitePWA({
+      workbox: {
+        // 20 MiB, adjust as you like
+        maximumFileSizeToCacheInBytes: 20 * 1024 * 1024,
+        // ...any other workbox options you already have
+      },
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
